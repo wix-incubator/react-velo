1aad525ce787fc76fee588d44e145e56eba2e073
Yury Michurin <yurym@wix.com>
2023-03-22 11:14:36 +0200

    Hoist functions

--- packages/reconciler/__tests__/e2e/element-getter-sanity.spec.ts
+++ packages/reconciler/__tests__/e2e/element-getter-sanity.spec.ts
@@ -1,6 +1,17 @@
 import puppeteer from 'puppeteer';
 import { getPageAtUrl } from './browser-utils';
 
+const waitForTextToBe = (value: string) => {
+    const valueOnPage = Array.from(document.querySelectorAll('[data-testid="richTextElement"]'))
+    .map(el => (el as HTMLElement).innerText).join('').trim();
+
+    return value === valueOnPage;
+}
+
+const getRadioInputValues = () => {
+    return Array.from(document.querySelectorAll('input[type=radio]')).map(el => (el as HTMLInputElement).value);
+};
+
 describe('getter setter sanity', () => {
     let browser: puppeteer.Browser;
     beforeAll(async () => {
@@ -11,17 +22,6 @@ describe('getter setter sanity', () => {
     }, 30000);
 
     it('radioGroup getter shouldnt interfere with setting the property', async () => {
-        const waitForTextToBe = (value: string) => {
-            const valueOnPage = Array.from(document.querySelectorAll('[data-testid="richTextElement"]'))
-            .map(el => (el as HTMLElement).innerText).join('').trim();
-
-            return value === valueOnPage;
-        }
-
-        const getRadioInputValues = () => {
-            return Array.from(document.querySelectorAll('input[type=radio]')).map(el => (el as HTMLInputElement).value);
-        };
-
         const page = await getPageAtUrl(browser, 'https://yurym4.wixsite.com/react-velo-e2e/velo-element-getter-sanity');
 
         await page.waitForFunction(waitForTextToBe, {}, "ready");
